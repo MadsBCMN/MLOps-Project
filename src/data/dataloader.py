2c3e75b22635e698d39f2fb032f756c68c1c7f69
cOlterO <43588141+cOlterO@users.noreply.github.com>
2024-01-06 18:46:30 +0100

    logging, hydra, wandb (#2)

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -1,7 +1,14 @@
 import torch
+import os
+import sys
+
+
 
 def dataloader():
     """Return train and test dataloaders for your MRI dataset."""
+    # Set the working directory to the current directory
+    os.chdir(sys.path[0])
+
     # Load training and testing data
     train_data = torch.load(os.path.normpath("../data/processed/training_images.pt")).float()
     train_labels = torch.load(os.path.normpath("../data/processed/training_labels.pt"))
--- src/train_model.py
+++ src/train_model.py
@@ -1,56 +1,79 @@
 import os
 import sys
+import logging
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
-import timm
+from torch.utils.data import DataLoader
 from models.model import model
 from data.dataloader import dataloader
+from omegaconf import OmegaConf
+import hydra
+import wandb
 
 # Set the working directory to the current directory
 os.chdir(sys.path[0])
 
-# Load your datasets
-train_dataset, test_dataset = dataloader()
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+# setup logging
+log = logging.getLogger(__name__)
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+@hydra.main(config_path="config", config_name="config.yaml")
+def train(config):
+    hparams = config.hyperparameters
+    run = wandb.init(project="MLOps_project", config=OmegaConf.to_container(hparams, resolve=True, throw_on_missing=True))
+    torch.manual_seed(hparams["seed"])
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
 
-num_epochs = 4  # Define the number of training epochs
+    # Setup wandb model logging
+    wandb.watch(model, log_freq=100)
 
-for epoch in range(num_epochs):
-    # Training Phase
-    model.train()
-    total_loss = 0
-    for images, labels in train_loader:
-        # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
+    # Load your datasets
+    train_dataset, test_dataset = dataloader()
+    train_loader = DataLoader(train_dataset, batch_size=hparams["batch_size"], shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=hparams["batch_size"], shuffle=False)
 
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=hparams["lr"])
 
-        total_loss += loss.item()
+    for epoch in range(hparams["n_epochs"]):
+        # Training Phase
+        model.train()
+        total_loss = 0
+        for images, labels in train_loader:
+            # Forward pass
+            outputs = model(images.to(device))
+            loss = criterion(outputs, labels.to(device))
+            run.log({"loss": loss.item()})
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-    print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {total_loss/len(train_loader)}")
+            total_loss += loss.item()
 
-    # Evaluation Phase
-    model.eval()
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for images, labels in test_loader:
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+        log.info(f"Epoch [{epoch+1}/{hparams['n_epochs']}], Loss: {total_loss/len(train_loader)}")
 
-    accuracy = 100 * correct / total
-    print(f'Accuracy on the test set after epoch {epoch+1}: {accuracy}%')
+        # Evaluation Phase
+        model.eval()
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for images, labels in test_loader:
+                outputs = model(images.to(device))
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels.to(device)).sum().item()
 
-torch.save(model.state_dict(), '../models/model.pt')
+        accuracy = 100 * correct / total
+        run.log({"accuracy": accuracy})
+        log.info(f'Accuracy on the test set after epoch {epoch+1}: {accuracy}%')
+
+    torch.save(model.state_dict(), '../models/model.pt')
+    run.log_model(path='../models/model.pt', name="resnet18")
+    log.info("Model saved")
+
+    run.finish()
+
+if __name__ == "__main__":
+    train()
