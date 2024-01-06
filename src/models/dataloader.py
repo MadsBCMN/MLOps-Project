7ff67ea370e62ff732a784f870a13ca9b927bae5
mads.brodthagen <mads.brodthagen@cmnavigator.com>
2024-01-06 13:20:51 +0100

    Removed folders

--- src/models/dataloader.py
+++ src/models/dataloader.py
@@ -13,17 +13,9 @@ def mnist():
     test_data = torch.load(r"/Users/helenehjort/Library/Mobile Documents/com~apple~CloudDocs/Human-centered AI/9. semester/02476 Machine Learning Operations/Project/MLOps-Project/data/proces/testing_images.pt").float()
     test_labels = torch.load(r"/Users/helenehjort/Library/Mobile Documents/com~apple~CloudDocs/Human-centered AI/9. semester/02476 Machine Learning Operations/Project/MLOps-Project/data/proces/testing_labels.pt")
 
-    print(train_data.shape)
-    print(train_labels.shape)
-    print(test_data.shape)
-    print(test_labels.shape)
-
     # Unsqueeze the data tensors to add a channel dimension
     train_data = train_data.unsqueeze(1)
     test_data = test_data.unsqueeze(1)
-
-    print(train_data.shape)
-    print(test_data.shape)
 
     # Create TensorDatasets
     train_dataset = torch.utils.data.TensorDataset(train_data, train_labels)
