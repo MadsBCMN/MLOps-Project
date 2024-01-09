a18a0f2048162bbae7db8b11ba7a820d624de2d6
mads.brodthagen <mads.brodthagen@cmnavigator.com>
2024-01-09 13:15:47 +0100

    updated test

--- tests/test_model.py
+++ tests/test_model.py
@@ -6,18 +6,21 @@ from torchvision import transforms
 from PIL import Image
 import timm
 import numpy as np
-_TEST_ROOT = os.path.dirname(__file__)
-_PROJECT_ROOT = os.path.dirname(_TEST_ROOT)
-sys.path.append(_PROJECT_ROOT)
+sys.path.append(os.path.normcase(os.getcwd()))
 from src.models.model import timm_model
+
 
 def load_model(model_path):
     model = timm_model()
     # Load the saved model weights
-    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    state_dict = torch.load("models/model.pt")
+    state_dict = {k.partition('model.')[2]:state_dict[k] for k in state_dict.keys()}
+    model.load_state_dict(state_dict)
     model.eval()
     return model
 
 def test_model():
     model = load_model("models/model.pt")
     assert model(torch.rand(1,1,86,86)).size() == (1,4) , "The output shape is not correct. Expected four categories"
+
+test_model()
