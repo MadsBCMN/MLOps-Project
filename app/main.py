19206c3748ba5b57bbdc996819744d08c292ba30
Steffen <s184208@student.dtu.dk>
2024-01-15 14:55:36 +0100

    change back to new backend

--- app/main.py
+++ app/main.py
@@ -12,22 +12,34 @@ import torch
 import torch.nn as nn
 import timm
 from fastapi import HTTPException
+from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_client import Counter
+
+sys.path.append(os.path.normcase(os.getcwd()))
+
 ### MODEL ####
 # Global variables
 MODEL_NAME = 'resnet18'
 NUM_CLASSES = 4
+
 def timm_model() -> nn.Module:
     """
     Creates a custom model based on timm's resnet18 with modified output layer.
+
     Returns:
         nn.Module: Custom model.
     """
     # Load pre-trained resnet18 model
     model = timm.create_model(MODEL_NAME, pretrained=True, in_chans=1)
+
     # Modify the output layer to match the number of classes
     model.fc = nn.Linear(model.fc.in_features, NUM_CLASSES)
+
     return model
-#####################
+####################
+# Create a counter metric to track image classification requests
+image_classification_requests = Counter('image_classification_requests_total', 'Total number of image classification requests')
+
 app = FastAPI()
 CLASS_LABELS = ['glioma', 'meningioma', 'no_tumor', 'pituitary']
 def load_model(model_path):
@@ -38,38 +50,49 @@ def load_model(model_path):
     model.eval()
     return model
 
-model_path = "../models/model.pt"
 model_path = os.path.abspath('models/model.pt')
 try:
     model = load_model(model_path)
 except Exception as e:
     raise RuntimeError(f"Error loading the model: {e}")
+
 def process_image(image: Image.Image) -> torch.Tensor:
     transform = transforms.Compose([
         transforms.Grayscale(num_output_channels=1),
         transforms.Resize((86, 86)),
     ])
+
     image = transform(image)
     image_array = np.array(image)
     image_tensor = torch.tensor(image_array, dtype=torch.float32)
     image_tensor = image_tensor.unsqueeze(0).unsqueeze(0)
     return image_tensor
+
 @app.post("/classify")
 async def classify_image(file: UploadFile = File(...)):
     try:
+        # Increment the image_classification_requests counter
+        image_classification_requests.inc()
+
         if not file.content_type or file.content_type.split("/")[0] != "image":
             raise HTTPException(status_code=400, detail="Invalid file type. Only images are supported.")
+
         contents = await file.read()
         image = Image.open(io.BytesIO(contents))
         processed_image = process_image(image)
         output = model(processed_image)
         _, predicted = torch.max(output.data, 1)
+
         # Map the predicted class index to the corresponding label
         predicted_label = CLASS_LABELS[predicted.item()]
-        return JSONResponse(content={"class": predicted_label}, status_code=200)
+
+        return JSONResponse(content={"class": predicted_label, "class_label": predicted.item()}, status_code=200)
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
+
+# Instrument the FastAPI application
+Instrumentator().instrument(app).expose(app)
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
